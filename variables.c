ce14ab4c9e6ef64bb3c9ee31d1061af46cb2bcd2
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-06-07 02:31:48 +0500

    Enhance variable declaration examples and add naming conventions in variables.c

--- variables.c
+++ variables.c
@@ -11,8 +11,27 @@ int main(void)
     * - type: The data type of the variable (e.g., int, float, char, etc.)
     */
     int a = 5;
+    int b = 10; 
 
-    printf("Hamza");
-    printf("%d", a);
+    char ch = 'A'; // Character variable
+
+    /*
+    * variables names should be meaningful and follow certain rules:
+    * - They must start with a letter (a-z, A-Z) or an underscore (_).
+    * - They can be followed by letters, digits (0-9), or underscores.
+    * - They cannot be a reserved keyword (e.g., int, return, if, etc.).
+    * - They are case-sensitive (e.g., myVar and myvar are different variables).
+    */
+
+    int myVariable = 20; // Valid variable name
+    // int 1stVariable = 30; // Invalid variable name (cannot start with a digit)
+    // int return = 40; // Invalid variable name (cannot be a reserved keyword)
+    // int myVar = 50; // Valid variable name
+    
+
+    printf("Hamza \n");
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%c \n", ch);
     return 0;
 }
