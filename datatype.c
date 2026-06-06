4684dd24fd4c12e2429b4e14e2049d035025c29e
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-06-07 02:37:48 +0500

    Add datatype.c with examples of C data types and variable declarations

---
+++ datatype.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // data types in c
+    /**
+     * In C, data types define the type of value a variable can hold and the operations that can be performed on it.
+     * The main data types in C are:
+     * - int: for integer values
+     * - float: for single-precision floating-point values
+     * - double: for double-precision floating-point values
+     * - char: for single characters
+     * - void: for no value
+     */
+
+     int myInt = 10; // integer variable
+     float myFloat = 3.14; // floating-point variable
+     double myDouble = 3.14159; // double-precision floating-point variable
+     char myChar = 'A'; // character variable
+     void *myVoidPtr; // void pointer
+
+    printf("hamza");
+    return 0;
+}
