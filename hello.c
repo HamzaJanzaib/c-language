10bca4a675d9540e7bc612dd133650773b126785
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-06-07 02:26:11 +0500

    Add initial C program and configuration for IntelliSense

---
+++ hello.c
@@ -0,0 +1,14 @@
+// hello.c
+#include <stdio.h>
+
+/*
+ * hello.c
+ * This is a simple C program that prints "hamza" to the console.
+ * It includes the standard input/output library to use the printf function.
+ */
+int main(void){
+    int a = 5; // This variable is declared but not used in the program.
+    printf("hamza \n"); // This line prints "hamza" followed by a newline character to the console.
+    printf("%d \n", a); // This line prints the value of variable 'a' followed by a newline character to the console.
+    return 0;
+}
