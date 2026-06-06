9c653943b7bc3233c0d0c8d6bdb9351ee244b919
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-06-07 02:28:56 +0500

    Add variables.c with variable declaration and initialization example

---
+++ variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Variable declaration and initialization
+    /*
+    * In C, a variable is a named storage location that can hold a value of a specific type.
+    * The syntax for declaring a variable is:
+    * type variable_name = value;
+    * Where:
+    * - type: The data type of the variable (e.g., int, float, char, etc.)
+    */
+    int a = 5;
+
+    printf("Hamza");
+    printf("%d", a);
+    return 0;
+}
